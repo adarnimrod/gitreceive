1e1f9009f3b952ed82ad82df79e5d8b85ec4bb74
Adar Nimrod <nimrod@shore.co.il>
2016-12-12 17:22:52 +0200

    - A more realistic receiver script, added tests to verify said script.

--- tests/files/receiver.sh
+++ tests/files/receiver.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
 set -eu
+
+# This part was copied verbatim from
+# https://github.com/progrium/gitreceive/wiki/TipsAndTricks
+fetch_submodules () {
+    # We reinitialize .git to avoid conflicts
+    rm -fr .git
+    # GIT_DIR is previously set by gitreceive to ".", we want it back to default
+    # for this
+    unset GIT_DIR
+    git init .
+
+    # We read the submodules from .gitmodules
+    git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+        while read path_key path
+        do
+            rm -fr $path
+            url_key=`echo $path_key | sed 's/\.path/.url/'`
+            url=`git config -f .gitmodules --get "$url_key"`
+            git submodule add $url $path
+        done
+}
+
 mkdir -p /var/tmp/gitreceive
-tar -xC /var/tmp/gitreceive -f -
-echo "OK"
+cd /var/tmp/gitreceive
+echo '----> Unpacking ...'
+tar -xf -
+if [ -f .gitmodules ]
+then
+    echo '----> Fetching submodules ...'
+    fetch_submodules
+fi
+if [ -f receiver ] && [ -x receiver ]
+then
+    echo '----> Running receiver ...'
+    ./receiver
+fi
+echo '----> OK.'
